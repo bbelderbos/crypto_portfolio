194190d275d3ae98a8c343b8373e80386a23b2f9
Jesse Brink <jbrink0925@gmail.com>
2021-03-22 23:53:28 -0400

    created a Portfolio class to handle most of the logic in the portfolio_page view. Modified views.py to match. Everything works as intended.

---
+++ portfolio/helpers/portfolio.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+from portfolio.models import PortfolioHoldings
+from portfolio.forms import PortfolioForm
+from portfolio.helpers.chart import portfolio_pie_chart as pie_chart
+from portfolio.helpers.coin_data import CoinData
+
+
+FormData = namedtuple('FormData', 'ticker num_coins coin_name type_')
+cg = CoinData()
+
+class Portfolio:
+    def __init__(self, user) -> None:
+        self.user = user
+    
+
+    def create_user_set(self, coin):
+        return PortfolioHoldings.objects.filter(person=self.user).filter(coin_name=coin).first()
+    
+
+    def no_user_coins(self, ticker, num_coins, usd_amt, coin_name, type_):
+        if type_.lower() != 'sell':
+            new_coin = PortfolioHoldings(coin_ticker=ticker, number_of_coins=num_coins,
+                        amount_in_usd=usd_amt, coin_name=coin_name, type=type_, person=self.user)
+            new_coin.save()
+
+    
+    def get_form_data(self, form):
+        return FormData(ticker=form.cleaned_data['coin_ticker'],
+                        num_coins=float(form.cleaned_data['number_of_coins']),
+                        coin_name=form.cleaned_data['coin_name'],
+                        type_=form.cleaned_data['type'])
+    
+
+    def delete_coin(self, user_coins, fields):
+        user_coins.filter(coin_ticker=fields.ticker).update(amount_in_usd=0.00, number_of_coins=0.00)
+        user_coins.filter(coin_ticker=fields.ticker).first().delete()
+    
+
+    def update_or_delete(self, fields, coin, user_coins, new_total, new_usd):
+        if fields.type_.lower() == 'sell':
+            if float(coin.number_of_coins) - fields.num_coins > 0:
+                user_coins.filter(coin_ticker=fields.ticker).update(number_of_coins=new_total,
+                                                                amount_in_usd=new_usd)
+            else:
+                self.delete_coin(user_coins, fields)
+        else:
+            user_coins.filter(coin_ticker=fields.ticker).update(number_of_coins=new_total, amount_in_usd=new_usd)
+    
+
+    def save_coin(self, fields, deleted_name, user, user_coins, amt_in_usd):
+        all_user_coins = [coin.coin_ticker for coin in user_coins.iterator()]
+        if fields.ticker not in all_user_coins and fields.ticker != deleted_name and fields.type_.lower() != 'sell':
+            new_coin = PortfolioHoldings(coin_ticker=fields.ticker, number_of_coins=fields.num_coins,
+            amount_in_usd=amt_in_usd, coin_name=fields.coin_name, type=fields.type_, person=user)
+            new_coin.save()
+    
+
+    def package_data_and_render(self, user_coins):
+        all_coins = {c.coin_ticker:float(c.amount_in_usd) for c in user_coins.iterator()}
+        data = [int(amt) for amt in all_coins.values()]
+        labels = [coin_ticker for coin_ticker in all_coins.keys()]
+        pie = pie_chart(data, labels)
+        display_coins = cg.portfolio_coins(self.user)
+        return pie, display_coins
--- portfolio/views.py
+++ portfolio/views.py
@@ -10,6 +10,7 @@ from .forms import PortfolioForm
 from portfolio.helpers.chart import chart_data, portfolio_pie_chart
 from portfolio.helpers.scrape_logos import scrape_coin_logos
 from portfolio.helpers.coin_data import CoinData
+from portfolio.helpers.portfolio import Portfolio
 
 cg = CoinData()
 
@@ -35,91 +36,31 @@ def portfolio_page(request):
     form = PortfolioForm()
     user = request.user
     user_coins = PortfolioHoldings.objects.filter(person=user)
+    pt = Portfolio(user)
 
     if request.method == 'POST':
-        user_set = PortfolioHoldings.objects.filter(person=user).filter(coin_name=request.POST['coin_name']).first()
+        user_set = pt.create_user_set(request.POST['coin_name'])
         form = PortfolioForm(request.POST, instance=user_set)
 
         if form.is_valid():
-            type_ = form.cleaned_data['type']
-            coin_name = form.cleaned_data['coin_name'].lower()
-            ticker = form.cleaned_data['coin_ticker']
-            number_of_coins = float(form.cleaned_data['number_of_coins'])
-            price = cg.single_coin_data(coin_name).price
-            amt_in_usd = number_of_coins * price
+            fields = pt.get_form_data(form)
+            price = cg.single_coin_data(fields.coin_name).price
+            amt_in_usd = fields.num_coins * price
             if not user_coins:
-                if type_.lower() != 'sell':
-                    new_coin = PortfolioHoldings(coin_ticker=ticker, number_of_coins=number_of_coins,
-                                amount_in_usd=amt_in_usd, coin_name=coin_name, type=type_, person=user)
-                    new_coin.save()
+                pt.no_user_coins(fields.ticker, fields.num_coins, amt_in_usd, fields.coin_name, fields.type_)
                 return HttpResponseRedirect('/portfolio')
             else:
                 for coin in PortfolioHoldings.objects.filter(person=user).iterator():
-                    new_coin_total = float(coin.number_of_coins) - number_of_coins
+                    new_coin_total = float(coin.number_of_coins) - fields.num_coins
                     new_usd_amt = new_coin_total * price
-                    if coin.coin_ticker == ticker:
-                        if type_.lower() == 'sell':
-                            if float(coin.number_of_coins) - number_of_coins > 0:
-                                user_coins.filter(coin_ticker=ticker).update(number_of_coins=new_coin_total,
-                                                                             amount_in_usd=new_usd_amt)
-                            else:
-                                deleted_name = coin.coin_ticker
-                                user_coins.filter(coin_ticker=ticker).update(amount_in_usd=0.00, number_of_coins=0.00)
-                                user_coins.filter(coin_ticker=ticker).first().delete()
-                        else:
-                            user_coins.filter(coin_ticker=ticker).update(number_of_coins=new_coin_total, amount_in_usd=new_usd_amt)
+                    if coin.coin_ticker == fields.ticker:
+                        deleted_name = coin.coin_ticker
+                        pt.update_or_delete(fields, coin, user_coins,
+                                            new_coin_total, new_usd_amt)
                 
-                all_user_coins = [coin.coin_ticker for coin in user_coins.iterator()]
-                if ticker not in all_user_coins and ticker != deleted_name and type_.lower() != 'sell':
-                    new_coin = PortfolioHoldings(coin_ticker=ticker, number_of_coins=number_of_coins,
-                    amount_in_usd=amt_in_usd, coin_name=coin_name, type=type_, person=user)
-                    new_coin.save()
-        
-        return HttpResponseRedirect('/portfolio')
-
-
+                pt.save_coin(fields, deleted_name, user, user_coins, amt_in_usd)
+            return HttpResponseRedirect('/portfolio')
     else:
-        all_coins = {c.coin_ticker:float(c.amount_in_usd) for c in user_coins.iterator()}
-        data = [int(amt) for amt in all_coins.values()]
-        labels = [coin_ticker for coin_ticker in all_coins.keys()]
-        pie = portfolio_pie_chart(data, labels)
-        display_coins = cg.portfolio_coins(user)
+        pie, display_coins = pt.package_data_and_render(user_coins)
         return render(request, 'portfolio.html', {'pie': pie, 'form': form, 'info': display_coins})
-
-# Comments and Explanation:
-
-# Lots to handle here. 
-
-# user_coins - set as variable to avoid typing out the same model query over and over.
-# user_set(lines 39 & 40) - When I added the UNIQUE constraint, it wouldn't let me add the same coin for a different
-#     user. By passing in the user_set as an instance, it seemed to only look for the coins that belonged 
-#     to that user, and therefore wouldn't update other users' coins. 
-
-# Lines 47-56: If the user didn't already have coins, I had to handle two scenarios. 1). The user accidentally entered Sell, 
-# which doesn't make sense....you can't have negative coins. So we only want to save the data if the user enters Buy if 
-# they don't already have coins in the system. So if they don't have coins and they didn't enter sell, we save the data
-# and return the template. If they enter Sell, we just return the template.
-
-
-# Lines 57-87: They already have coins in the system. We have to go through their holdings coin by coin and check to see 
-# if they coin they entered on the form is in their holdings. If it is, we update it. Then we have to handle Buy or Sell
-# there as well. If Sell, we have to update the number of coins and the usd amount. If Buy, we also update, but the update 
-# is simpler. 
-
-# I ran into some weird behavior with deleting a coin which is why I needed lines 69-73 and line 80. Basically, it would
-# delete the entry, but if I bought again, the old data would reappear and it would say I had the old data + the new data. 
-# Running lines 69-73 and the conditional check on line 80 took care of this.
-
-# Lines 79-83: If the user has coins in the system, but not the coin they entered (and it wasn't deleted above...) we 
-# save the data. 
-
-# Lines 85-87: The return statement for if the request is POST and the user has coins. 
-
-# Lines 90-93: Return statement for it it's a GET request. 
-
-# I had to use HttpResponseRedirect for the POST request returns because without it, if I just refreshed the page,
-# it would submit the form again, and update user data when I don't want it to. 
-
-# You can ignore the "pie" and "all_coins" for now. I need to work on them and they are just there as placeholders so that 
-# when I run the server, I see what the page should look like. 
-
+                        
