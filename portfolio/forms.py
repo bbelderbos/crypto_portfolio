86bd400c647af03ab86b41ca2e9cbd36c13c1d98
Jesse Brink <jbrink0925@gmail.com>
2021-03-25 20:14:39 -0400

    Was able to tinker with the default in the model to allow the placeholder text to show in the form (no more erasing 'Enter Your Ticker Here'), and was also able to add a pulldown select field to the form to make it more user friendly. Also accounted for what might happen if a user enters '-21.36' or a similar value.

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -4,10 +4,14 @@ from .models import PortfolioHoldings
 class PortfolioForm(forms.ModelForm):
     class Meta:
         model = PortfolioHoldings
+        TYPE_CHOICES = (
+            ('Buy', 'Buy'),
+            ('Sell', 'Sell')
+        )
         widgets = {
             'coin_ticker': forms.TextInput(attrs={'placeholder': 'Coin (Ticker Symbol)'}),
             'number_of_coins': forms.TextInput(attrs={'placeholder': 'Enter Amount of Coins Bought'}),
             'coin_name': forms.TextInput(attrs={'placeholder': 'Enter the coin name'}),
-            'type': forms.TextInput(attrs={'placeholder': 'Buy or Sell'})
+            'type': forms.Select(choices=TYPE_CHOICES, attrs={'placeholder': 'Buy or Sell'})
         }
         fields = ['coin_ticker', 'number_of_coins', 'coin_name', 'type']
