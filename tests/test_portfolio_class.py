65a856ffb291f6ba776d2f840997685807bac7ea
Jesse Brink <jbrink0925@gmail.com>
2021-03-29 22:48:03 -0400

    Made some changes to the test_portfolio_class.py file

--- tests/test_portfolio_class.py
+++ tests/test_portfolio_class.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from unittest.mock import Mock, patch
 from typing import List, Sequence, Any
+from django.db.models import query
 
 import pytest
 from django.contrib.auth.models import User
@@ -19,42 +20,36 @@ cg = CoinData()
 def create_user_and_class_instance():
     user = User.objects.create_user(username='joe', password='tesTing123%')
     pt = Portfolio(user)
-    return user, pt
+    coin = PortfolioHoldings.objects.create(coin_ticker='NEO', number_of_coins='51',
+                            amount_in_usd='2000.00', coin_name='neo', type='Buy',
+                            person=user)
+    query = PortfolioHoldings.objects.filter(person=user)
+    return user, pt, query
 
 
 @pytest.mark.parametrize('ticker, num_coins, usd_amt, coin_name, type_', [
     ('ICX', 26000.326, 52000.35, 'icon', 'Buy'),
     ('DOT', 300, 9000, 'DOT', 'Sell')
 ])
-
-
 @pytest.mark.django_db(transaction=True)
 def test_no_user_coins(ticker, num_coins, usd_amt, coin_name, type_):
-    user = User.objects.create_user(username='joe', password='tesTing123%')
-    pt = Portfolio(user)
+    user, pt, query = create_user_and_class_instance()
     pt.no_user_coins(ticker, num_coins, usd_amt, coin_name, type_)
-    query = PortfolioHoldings.objects.filter(person=user, coin_ticker=ticker).first()
-
-    if ticker == 'ICX':
-        assert query.coin_ticker == 'ICX'
-    else:
-        assert query is None
+    query = query.filter(coin_ticker=ticker).first()
+    assert query is None or query.coin_ticker == 'ICX'
 
 
 @pytest.mark.parametrize('ticker, num_coins, usd_amt, coin_name, type_', [
     ('ICX', 26000.326, 52000.35, 'icon', 'Buy'),
     ('DOT', 300, 9000, 'DOT', 'Sell')
 ])
-
-
 @pytest.mark.django_db(transaction=True)
 def test_delete_coin(ticker, num_coins, usd_amt, coin_name, type_):
-    user = User.objects.create_user(username='joe', password='tesTing123%')
-    pt = Portfolio(user)
+    user, pt, query = create_user_and_class_instance()
     PortfolioHoldings.objects.create(coin_ticker=ticker, number_of_coins=num_coins,
                             amount_in_usd=usd_amt, coin_name=coin_name, type=type_,
                             person=user)
-    query = PortfolioHoldings.objects.filter(person=user)
+
     fields = Fields(ticker=ticker, num_coins=num_coins, usd_amt=usd_amt,
                     coin_name=coin_name, type_=type_)
     pt.delete_coin(query, fields)
@@ -65,31 +60,23 @@ def test_delete_coin(ticker, num_coins, usd_amt, coin_name, type_):
     ('ICX', 26000.326, 52000.35, 'icon', 'Buy'),
     ('DOT', 300, 9000, 'DOT', 'Sell')
 ])
-
-
 @pytest.mark.django_db(transaction=True)
 def test_update_or_delete_coin(ticker, coin_amt, usd_amt, coin_name, type_):
-    user, pt = create_user_and_class_instance()
-    if ticker == 'ICX':
-        num_coins = 10000
-    else:
-        num_coins = 200
+    user, pt, query = create_user_and_class_instance()
+    num_coins = 10000 if ticker == 'ICX' else 200
     PortfolioHoldings.objects.create(coin_ticker=ticker, number_of_coins=num_coins,
                             amount_in_usd=usd_amt, coin_name=coin_name, type=type_,
                             person=user)
-    query = PortfolioHoldings.objects.filter(person=user)
+    
     instance = query.filter(coin_ticker=ticker).first()
     fields = Fields(ticker=ticker, num_coins=coin_amt, usd_amt=usd_amt,
                     coin_name=coin_name, type_=type_)
     new_total = num_coins - coin_amt if fields.type_ == 'Sell' else num_coins + coin_amt
-    print(new_total)
     
     pt.update_or_delete(fields, instance, query, new_total, 25000)
     coin = query.filter(coin_ticker=ticker).first()
-    if ticker == 'ICX':
-        assert float(coin.number_of_coins) == 36000.326
-    else:
-        assert coin is None
+    assert coin is None or float(coin.number_of_coins) == 36000.326
+    
 
 
 @pytest.mark.parametrize('amt_in_usd, fields', [
@@ -97,14 +84,12 @@ def test_update_or_delete_coin(ticker, coin_amt, usd_amt, coin_name, type_):
     Fields(ticker='ETH', num_coins=200, usd_amt=15000, coin_name='ethereum',
     type_='Sell')),
     (35000,
-    Fields(ticker='NEO', num_coins=200, usd_amt=15000, coin_name='neo',
+    Fields(ticker='RSR', num_coins=200, usd_amt=15000, coin_name='neo',
     type_='Buy'))
 ])
-
-
 @pytest.mark.django_db(transaction=True)
 def test_save_new_coin(amt_in_usd, fields):
-    user, pt = create_user_and_class_instance()
+    user, pt, query = create_user_and_class_instance()
     if fields.ticker == 'ETH':
         PortfolioHoldings.objects.create(coin_ticker='ETH', number_of_coins=200,
                             amount_in_usd=15000, coin_name='ethereum', type='Sell',
@@ -114,10 +99,7 @@ def test_save_new_coin(amt_in_usd, fields):
                             amount_in_usd=15000, coin_name='aave', type='Buy',
                             person=user)
 
-    user_coins = PortfolioHoldings.objects.filter(person=user)
-    pt.save_new_coin(fields, user, user_coins, amt_in_usd)
-    coin = PortfolioHoldings.objects.filter(person=user, coin_ticker=fields.ticker).first()
-    if fields.ticker == 'ETH':
-        assert float(coin.amount_in_usd) == 15000
-    else:
-        assert float(coin.amount_in_usd) == 35000.00
+    pt.save_new_coin(fields, user, query, amt_in_usd)
+    coin = query.filter(coin_ticker=fields.ticker).first()
+    assert float(coin.amount_in_usd) in {15000, 35000.00}
+    
